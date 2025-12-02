16a0a551949826bdf54377386acc96f58dfd6ae4
diegoteis1222 <diegoteis122@gmail.com>
2025-12-02 14:29:02 +0100

    piton

--- python_web_server/main.py
+++ python_web_server/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, jsonify, render_template_string, request
 
 app = Flask(__name__)
 
@@ -12,10 +12,41 @@ def saludar():
 
 @app.route('/suma')
 def suma():
-    a = request.args.get('a')
-    b = request.args.get('b')
+    a = request.args.get('a', type=int, default=0)
+    b = request.args.get('b', type=int, default=0)
     resultado =  a + b
-    return "la suma es " + resultado
+    return f"La suma de {a} + {b} es: {resultado}"
+
+form_html = '''
+<form method="POST" action="/login">
+    Usuario: <input type="text" name="usuario"><br>
+    Contraseña: <input type="password" name="password"><br>
+    <input type="submit" value="Login">
+</form>
+'''
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template_string(form_html)
+    elif request.method == 'POST':
+        usuario = request.form.get('usuario', default='')
+        password = request.form.get('password', default='')
+        if usuario == 'root' and password == '1234':
+            return "Login exitoso", 200
+        else:
+            return "Usuario o contraseña incorrectos", 401
+    else:
+        pass
+    
+@app.route('/api/suma' , methods=['POST'])
+def api_suma():
+    data = request.get_json()
+    num1 = data.get('num1', 0)
+    num2 = data.get('num2', 0)
+    resultado =  num1 + num2
+    diccionario = {"resultado": resultado}
+    return jsonify(diccionario)
 
 if __name__ == "__main__":
     app.run()
